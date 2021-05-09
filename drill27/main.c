f2f7884723447334f3e60175d96f8d9a08725fea
Fradi575 <78791893+Fradi575@users.noreply.github.com>
2021-05-09 12:12:37 +0200

    drill27 feltöltése
    drill27 feltöltése

---
+++ drill27/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    printf("Hello world!\n");
+    char hello[6] = "Hello " ;
+    char world[6] = "World!" ;
+    printf("%s\n",hello);
+    int x = 7;
+    char* p = "foo";
+    printf("%s\n", p);
+    printf("%i\n", x);
+    return 0;
+}
